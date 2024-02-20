bf74287d1f33cada960ca3f8184ad36632160377
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-02-20 18:59:58 +0500

    upda

--- api/index.js
+++ api/index.js
@@ -840,7 +840,7 @@ app.post('/api/testersuccessuser', async (req, res) => {
     const coursenameid = req.body.courseEnrollments[0].courseId
     // Perform student creation or update logic here
     const existingStudent = await studentModel.findOne({ Email: req.body.Email });
-const courseidbypurchase = await Course.findById({coursenameid})
+const courseidbypurchase = await Course.findById({_id: coursenameid})
  if(courseidbypurchase){
   var CourseName = courseidbypurchase.courseName
  }
